fb124887857d1bb0c4a17f14a6da6bd5705c3b78
Hyo Seong Choi <rorlab@gmail.com>
2014-06-14 17:51:35 +0900

    Devise + Rolify + Authority

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout :dynamic_layout
+
+  def authority_forbidden(error)
+    Authority.logger.warn(error.message)
+    redirect_to request.referrer.presence || root_path, :alert => 'You are not authorized to complete that action.'
+  end
+
+  private
+  def dynamic_layout
+    devise_controller? ? 'devise_layout' : 'general_layout'
+  end
+
 end
