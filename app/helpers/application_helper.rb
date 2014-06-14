4d72c2a90c7b69a3bf6fc59c4195d061c5b36427
Hyo Seong Choi <rorlab@gmail.com>
2014-06-14 17:51:35 +0900

    Devise + Rolify + Authority

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,32 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    case flash_type
+      when "success"
+        "alert-success"   # Green
+      when "error"
+        "alert-danger"    # Red
+      when "alert"
+        "alert-warning"   # Yellow
+      when "notice"
+        "alert-info"      # Blue
+      else
+        flash_type.to_s
+    end
+  end
+
+  def icon(shape)
+    "<span class='glyphicon glyphicon-#{shape}'></span>".html_safe
+  end
+
+  def active_menu(target_controller)
+    "class=#{controller_name == target_controller ? 'active' : ''}"
+  end
+
+  def user_email(post)
+    post.user.present? ? post.user.email : "an anonymous user"
+  end
+
+  def user_roles(user)
+    user.roles.map(&:name).join(', ').titleize
+  end
 end
