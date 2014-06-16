d49b580bc27a260fec20e6d2c35d3a84c447a74e
Hyo Seong Choi <rorlab@gmail.com>
2014-06-17 08:35:12 +0900

    중복 마이그레이션 파일 삭제함.

--- db/migrate/20140616231316_add_title_to_post.rb
+++
@@ -1,8 +0,0 @@
-class AddTitleToPost < ActiveRecord::Migration
-  def change
-    add_column :posts, :title, :string
-    add_column :posts, :content, :text
-    add_reference :posts, :writer, index: true
-    add_column :posts, :published, :boolean, default: false
-  end
-end
--- db/migrate/20140616231320_add_reference_to_answer.rb
+++
@@ -1,5 +0,0 @@
-class AddReferenceToAnswer < ActiveRecord::Migration
-  def change
-    add_reference :answers, :question, index: true
-  end
-end
