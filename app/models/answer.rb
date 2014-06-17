000df461176716785ab7bffa898fc7500bae1223
Hyo Seong Choi <rorlab@gmail.com>
2014-06-17 10:06:36 +0900

    TDD 테스트 All Passed~

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -11,10 +11,9 @@
 #
 
 class Answer < ActiveRecord::Base
-  validates_presence_of :content
-
-  has_many :comments, :as => :commentable, :dependent => :destroy
-
   belongs_to :question
   belongs_to :user
+  has_many :comments, :as => :commentable, :dependent => :destroy
+
+  validates :content, presence: true
 end
