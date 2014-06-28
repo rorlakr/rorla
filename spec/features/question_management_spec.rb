02c25c4d3c8c824a14fa8f9d259707aaaf4db41e
plaredspear <plaredspear@gmail.com>
2014-06-28 20:11:48 +0900

    Question 수정, 삭제

--- spec/features/question_management_spec.rb
+++ spec/features/question_management_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 Capybara.javascript_driver = :webkit
 
-feature '질문을 관리한다.' do
+feature '질문을 관리한다.', js: true do
   background do
     @user = create(:user, email: 'user@example.com', password: 'testtest', id: 1)
     @user.confirm!
@@ -24,6 +24,8 @@ feature '질문을 관리한다.' do
 
     visit '/'
 
+    click_link 'Bulletins'
+
     click_link 'Q&A'
   end
 
@@ -43,7 +45,7 @@ feature '질문을 관리한다.' do
   scenario '질문을 등록한다.' do
     click_link '질문 등록'
 
-    fill_in '질문', with: '첫번째 질문입니다.'
+    fill_in '제목', with: '첫번째 질문입니다.'
     fill_in '내용', with: '첫번째 질문입니다. 이렇게 하면 될까요?'
 
     click_button '등록'
@@ -53,17 +55,63 @@ feature '질문을 관리한다.' do
 
   scenario '질문을 수정한다.' do
     within('.item-1') do
-      click_link '수정'
+      click_link '보기'
     end
 
-    expect(find_field('질문').value).to eq('I have Question')
+    click_link '수정'
+
+    expect(find_field('제목').value).to eq('I have Question')
     expect(find_field('내용').value).to eq('If you..')
 
-    fill_in '질문', with: '한글로 질문'
+    fill_in '제목', with: '한글로 질문'
+    fill_in '내용', with: '어떻게 하면 개발을 잘 할 수 있을까요?'
 
     click_button '갱신'
 
     expect(page).to have_content('Question was successfully updated.') 
   end
+
+  scenario '질문 정보를 입력하지 않아 수정에 실패한다.' do
+    within('.item-1') do
+      click_link '보기'
+    end
+
+    click_link '수정'
+
+    expect(find_field('제목').value).to eq('I have Question')
+    expect(find_field('내용').value).to eq('If you..')
+
+    fill_in '제목', with: ' '
+    fill_in '내용', with: ' '
+
+    click_button '갱신'
+    expect(page).to have_content('제목에 내용을 입력해 주세요')
+  end
+
+  scenario '질문을 삭제한다.' do
+    within('.item-1') do
+      click_link '보기'
+    end
+
+    page.driver.accept_js_confirms!
+
+    click_link '삭제'
+
+    expect(page).to_not have_content('I have Question')
+  end
+
+  scenario '질문을 삭제하지 않는다.' do
+    within('.item-1') do
+      click_link '보기'
+    end
+
+    page.driver.dismiss_js_confirms!
+
+    click_link '삭제'
+
+    expect(page).to have_content('I have Question')
+  end
+
+
 
 end
