edc9a2904f4b173fd80e82403be8c4619aa50195
plaredspear <plaredspear@gmail.com>
2014-07-13 16:36:23 +0900

    QNA 유저 연동

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140618121658) do
+ActiveRecord::Schema.define(version: 20140713070735) do
 
   create_table "answers", force: true do |t|
     t.text     "content"
