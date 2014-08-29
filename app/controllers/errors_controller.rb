2bd33b6747896d435b8bac733daa7aa39be77c67
Hyo Seong Choi <hschoidr@gmail.com>
2014-08-29 20:18:47 +0900

    서버 에러 교정함.
    ErrorController를 ApplicationController로부터 상속 받도록 했음.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,8 +1,8 @@
-class ErrorsController < ActionController::Base
+class ErrorsController < ApplicationController
   def not_found
     render status: :not_found
   end
- 
+
   def server_error
     render status: :server_error
   end
