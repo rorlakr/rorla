a5ff06c69100ad1a10fdaaba68c507610556651f
seapy <iamseapy@gmail.com>
2014-09-13 12:50:10 +0900

    Podcast 목록 구현
    - podcasts 테이블에 uid, published_at 추가
    - 팟캐스트 json에서 데이터를 파싱하기 위한 task 및 관련 gem 추가

--- db/migrate/20140913024616_create_podcasts.rb
+++ db/migrate/20140913024616_create_podcasts.rb
@@ -1,12 +1,14 @@
 class CreatePodcasts < ActiveRecord::Migration
   def change
     create_table :podcasts do |t|
+      t.string :uid
       t.string :title
       t.string :author
       t.text :description
       t.text :enclosure_url
       t.integer :enclosure_size, default: 0
       t.string :duration
+      t.date :published_at
 
       t.timestamps
     end
