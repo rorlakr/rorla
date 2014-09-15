b7b7eaa3c17dd91ab76fd3fbe684280f348eaace
Hyo Seong Choi <hschoidr@gmail.com>
2014-09-16 08:26:33 +0900

    rBlog content 입력창의 autosize 기능 추가함.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require jquery.pageless
+//= require jquery.autosize
 //= require bootstrap
 //= require pagedown_bootstrap
 //= require google-code-prettify-rails/prettify
