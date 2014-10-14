925cec98b498221c37a453f3fa03f82b6aaa0627
Eugene Park <parkeugene7676@gmail.com>
2014-10-14 19:50:49 +0900

    get Facebook events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,10 @@
 class EventsController < ApplicationController
   def FBevents
+    uri = URI.parse('https://graph.facebook.com/v2.1/151742221561550/events?access_token=CAACEdEose0cBAJ7pTu6lSBugNDCzhV4XgQ21uIYdMOmInFDax14nZCJCJn5fjKwJ8lMnhQgSanZBEil064OABPOaOZCpYa7aCSEmlSqIl27Xh14iIDXFEKYBSvTUiKXwyewqR3dbYVD39dlgojTi2N5IGKtXXG1Flbmr5qU8liWUfwujvGZCa8Tz7hfDZASBKlMWtt2CyLT6J26ywHrEZC')
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl=true
+    http.ssl_version = :TLSv1
+    response = http.get(uri.request_uri)
+
   end
 end
