94811c0859bcb0c781b57944bcfb3c49f0dd2017
ChangHoon Jeong <iamseapy@gmail.com>
2014-11-30 15:10:00 +0900

    Weeklynews 컨트롤러 추가

---
+++ app/controllers/weeklynews_controller.rb
@@ -0,0 +1,14 @@
+class WeeklynewsController < ApplicationController
+  def index
+    @items = Weeklynews.order(sended_at: :desc).paginate(:page => params[:page], :per_page => 20)    
+  end
+
+  def grap
+    if current_user.has_role?(:admin)
+      PodcastJob.new.async.perform
+      flash.now[:notice] = "주간뉴스 수집요청 완료"
+    else
+      flash.now[:alert] = "admin만 가능한 기능입니다"
+    end
+  end
+end
---
+++ app/helpers/weeklynews_helper.rb
@@ -0,0 +1,2 @@
+module WeeklynewsHelper
+end
