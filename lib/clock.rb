30273d20a493030a34fa432858260c6553043573
ChangHoon Jeong <iamseapy@gmail.com>
2014-11-30 16:11:34 +0900

    주간뉴스를 주기적으로 수집하는 스케줄 추가

--- lib/clock.rb
+++ lib/clock.rb
@@ -10,4 +10,9 @@ module Clockwork
   every(1.hour, 'merit.hourly_cron_job') { 
     puts `bin/rake merit.hourly_cron_job`
   }
+
+  # 일주일에 한번 주간뉴스 업데이트
+  every(1.week, 'weeklynews.update', :at => ['Sat 10:20'], :tz => 'Seoul') {
+    Runners::WeeklynewsRunner.update
+  }
 end
