3246e33abd51c2e009ad05fbbb007dbb95cbad98
Eugene Park <parkeugene7676@gmail.com>
2015-01-06 19:34:14 +0900

    Add column event_url in Event

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141130061052) do
+ActiveRecord::Schema.define(version: 20150105204509) do
 
   create_table "answers", force: true do |t|
     t.text     "content"
