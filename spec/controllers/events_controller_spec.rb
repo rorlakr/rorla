bf1c1faad84d8a39e6945cd32aca6fa5ff97cdf0
Eugene Park <parkeugene7676@gmail.com>
2015-01-06 19:38:33 +0900

    Update event controller_spec

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,10 +1,23 @@
 require 'spec_helper'
 
-describe EventsController,focus:true do
- let(:http_response) {"OK"}
-  describe "GET 'FBevents'", :vcr do
-    it "returns http success" do
-      subject.FBevents.message.should == http_response
+describe EventsController, focus:true do
+
+  # describe "GET 'getEvents'"do
+  #   it "returns http success" do
+  #     #puts(subject.getEvents["data"][0]["start_time"])
+  #     expect(subject.getEvents('151742221561550/events?limit=22&')["data"].length).to eq(22)
+  #   end
+  # end
+  #
+  # describe "GET 'FBevent'" do
+  #   it "returns description" do
+  #     expect(subject.FBevent('560777114059299?')["description"].size).to eq(235)
+  #   end
+  # end
+
+  describe "GET 'meetup'", :vcr do
+    it "returns meetup" do
+      expect(subject.meetup["results"][0]["status"]).to eq("upcoming")
     end
   end
 
