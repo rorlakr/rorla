aeb511c96aab20dbad79412b4cd455997b7eeb34
ChangHoon Jung <iamseapy@gmail.com>
2016-01-04 21:53:48 +0900

    puma 웹서버 교체 및 capistrano 설정

---
+++ config/deploy.rb
@@ -0,0 +1,40 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :application, 'rorla'
+set :repo_url, 'git@github.com:rorlakr/rorla.git'
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/rorlab/www/rorla'
+
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# rbenv
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.0'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all
+
+# bundler
+set :bundle_jobs, 4
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
