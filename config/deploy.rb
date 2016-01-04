483b79e56ac4a009e9fc5793d1d831d594509927
ChangHoon Jung <iamseapy@gmail.com>
2016-01-04 23:40:30 +0900

    푸마 재시작시 오류 수정

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.0'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails puma}
+set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl}
 set :rbenv_roles, :all
 
 # bundler
