19cad9204fa20d20a9fb6f74957a3cd8f468c268
Hyo Seong Choi <lucius.choi@gmail.com>
2016-02-23 23:47:58 +0900

    production.log 로거 변경

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,7 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
+
 # rbenv
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.0'
