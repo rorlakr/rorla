0db28e7676a70547359170085c09cd900808a412
Hyo Seong Choi <lucius.choi@gmail.com>
2016-02-23 23:47:58 +0900

    production.log 로거 변경

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,8 +49,8 @@ Rails.application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups.
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-  config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+  config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  # config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
