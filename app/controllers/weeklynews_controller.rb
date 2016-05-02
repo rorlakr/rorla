aea83aa3fe0e3ee5d22c47aaa81f34c04100f6ac
Lucius Choi <lucius.choi@gmail.com>
2016-05-02 21:06:33 +0900

    주간뉴스 메타태그 업데이트함.

--- app/controllers/weeklynews_controller.rb
+++ app/controllers/weeklynews_controller.rb
@@ -1,6 +1,11 @@
 class WeeklynewsController < ApplicationController
   def index
-    @items = Weeklynews.order(sended_at: :desc).paginate(:page => params[:page], :per_page => 20)    
+    @items = Weeklynews.order(sended_at: :desc).paginate(:page => params[:page], :per_page => 20)
+    set_meta_tags og: {
+      title: "RORLab | 주간뉴스",
+      description: "지난 한주간에 걸친 루비와 레일스 관련 정보를 정리하여 매주 일요일 자정에 주간뉴스 구독자에게 이메일을 발송하고 있습니다. 구독신청은 http://weekly.rorlab.org 입니다.",
+      url: weeklynews_path
+    }, title: "RORLab | 주간뉴스"    
   end
 
   def grap
