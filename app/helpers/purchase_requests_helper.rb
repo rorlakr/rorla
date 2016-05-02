ebc18e06e38922fee199412c0b287f8574bfceee
Lucius Choi <lucius.choi@gmail.com>
2016-05-02 22:44:18 +0900

    공동구매 권한 업데이트

--- app/helpers/purchase_requests_helper.rb
+++ app/helpers/purchase_requests_helper.rb
@@ -1,7 +1,13 @@
 module PurchaseRequestsHelper
   def purchase_confirmed(request)
-    request.confirmed ?
-    link_to( "완료", confirm_purchase_path(request), data:{ confirm: "완료 취소하시겠습니까?"},  remote: true, class:'btn btn-success btn-xs') :
-    link_to( "미확인", confirm_purchase_path(request), data:{ confirm: "입금 확인을 원하십니까?"}, remote: true, class:'btn btn-danger btn-xs')
+    if current_user.can_confirm?(request)
+      request.confirmed ?
+      link_to( "완료", confirm_purchase_path(request), data:{ confirm: "완료 취소하시겠습니까?"},  remote: true, class:'btn btn-success btn-xs') :
+      link_to( "미확인", confirm_purchase_path(request), data:{ confirm: "입금 확인을 원하십니까?"}, remote: true, class:'btn btn-danger btn-xs')
+    else
+      request.confirmed ?
+      content_tag(:div, '완료', class:'btn btn-success btn-xs') :
+      content_tag(:div, '미확인', class:'btn btn-danger btn-xs')
+    end
   end
 end
