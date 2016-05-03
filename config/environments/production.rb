319c30dff31235b00cdaff1e0e2ffbdda104ba0e
Lucius Choi <lucius.choi@gmail.com>
2016-05-03 20:57:32 +0900

    환경설정 오류 수정

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,8 +73,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
-          api_key: "<%= ENV['MAILGUN_API_KEY'] %>",
-          domain: 'rorlab.org'
+    api_key: "<%= ENV['MAILGUN_API_KEY'] %>",
+    domain: 'rorlab.org'
   }
 
 
