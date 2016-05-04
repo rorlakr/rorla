ff93319c8207e3c48e4722257dba492ebe1f75b5
Lucius Choi <lucius.choi@gmail.com>
2016-05-04 15:03:21 +0900

    메타 태그 업데이트

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    set_meta_tags og: {
+      title: "RORLAB",
+      description: "RORLab에 오신 것을 환영합니다. Ruby on Rails Korean Study Group with free offline lectures",
+      url: root_url
+    }, title: 'RORLAB'
   end
 end
