b48d54b5003b05e432488ef825f7e967ed065625
Lucius Choi <lucius.choi@gmail.com>
2016-05-04 16:11:53 +0900

    공동구매 인덱스 페이지 오픈

--- app/helpers/purchase_requests_helper.rb
+++ app/helpers/purchase_requests_helper.rb
@@ -1,6 +1,6 @@
 module PurchaseRequestsHelper
   def purchase_confirmed(request)
-    if current_user.can_confirm?(request)
+    if user_signed_in? && current_user.can_confirm?(request)
       request.confirmed ?
       link_to( "완료", confirm_purchase_path(request), data:{ confirm: "완료 취소하시겠습니까?"},  remote: true, class:'btn btn-success btn-xs') :
       link_to( "미확인", confirm_purchase_path(request), data:{ confirm: "입금 확인을 원하십니까?"}, remote: true, class:'btn btn-danger btn-xs')
