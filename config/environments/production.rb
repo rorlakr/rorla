3a8f36040ae0d9147d100dc692304cc36adb054b
Lucius Choi <lucius.choi@gmail.com>
2016-05-07 07:52:17 +0900

    로그 설정 오류 수정

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   # Set to :debug to see everything in the log.
   # config.log_level = :info
   config.log_level = :debug
-  Rails.logger.debug "Year: #{Time.now.year}"
+  config.logger.debug "Year: #{Time.now.year}"
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
