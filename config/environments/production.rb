8f6c9e9ed23f7e8b00564b71bab22131f3e5bd95
Lucius Choi <lucius.choi@gmail.com>
2016-05-07 08:04:47 +0900

    log_level 추가

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ Rails.application.configure do
 
   # Set to :debug to see everything in the log. (default => :info)
   # config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
