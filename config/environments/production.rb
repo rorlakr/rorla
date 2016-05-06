6b77e1a455f2e2eaa2f48ec0e5d9cd2d5764ed97
Lucius Choi <lucius.choi@gmail.com>
2016-05-07 08:02:14 +0900

    운영모드 로그 디폴트 전환

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,10 +42,8 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # Set to :debug to see everything in the log.
-  # config.log_level = :info
-  config.log_level = :debug
-  config.logger.debug "Year: #{Time.now.year}"
+  # Set to :debug to see everything in the log. (default => :info)
+  # config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
