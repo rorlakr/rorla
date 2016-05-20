943fbd6646d5485e4bb52ace7c5426b973889b74
Lucius Choi <lucius.choi@gmail.com>
2016-05-20 16:27:54 +0900

    일정 권한 조정

--- app/authorizers/applier_authorizer.rb
+++ app/authorizers/applier_authorizer.rb
@@ -1,15 +1,15 @@
 class ApplierAuthorizer < ApplicationAuthorizer
 
   def self.creatable_by?(user)
-    user.has_role?(:user) || user.has_role?(:admin)
+    user.has_role?(:user) || user.has_role?(:admin) || user.has_role?(:contributor)
   end
 
   def updatable_by?(user)
-    resource.user == user || user.has_role?(:admin)
+    resource.user == user || user.has_role?(:admin) || user.has_role?(:contributor)
   end
 
   def deletable_by?(user)
-    resource.user == user || user.has_role?(:admin)
+    resource.user == user || user.has_role?(:admin) || user.has_role?(:contributor)
   end
 
 end
--- app/authorizers/schedule_authorizer.rb
+++ app/authorizers/schedule_authorizer.rb
@@ -1,19 +1,19 @@
 class ScheduleAuthorizer < ApplicationAuthorizer
 
   def self.creatable_by?(user)
-    user.has_role?(:admin)
+    user.has_role?(:admin) || user.has_role?(:contributor)
   end
 
   def readable_by?(user)
-    user.has_role?(:user) || user.has_role?(:admin)
+    user.has_role?(:user) || user.has_role?(:admin) || user.has_role?(:contributor)
   end
 
   def updatable_by?(user)
-    user.has_role?(:admin)
+    user.has_role?(:admin) || user.has_role?(:contributor)
   end
 
   def deletable_by?(user)
-    user.has_role?(:admin)
+    user.has_role?(:admin) || user.has_role?(:contributor)
   end
 
 end
