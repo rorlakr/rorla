36f4a74e476c995a373913e3fcc1767312677ce2
Lucius Choi <lucius.choi@gmail.com>
2016-05-23 19:34:22 +0900

    bcc 추가

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -14,17 +14,20 @@ class UserMailer < ActionMailer::Base
   def confirm_application(user, applier)
     @applier = applier
     mail(:to      => user.email,
+         :bcc => User.with_role(:contributor).pluck(:email),
          :subject => "[RORLAB] #{@applier.schedule.name} 참가신청 완료")
   end
 
   def confirm_application_withdraw(user)
     mail(:to      => user.email,
+         :bcc => User.with_role(:contributor).pluck(:email),
          :subject => "[RORLAB] 참가신청 취소")
   end
 
   def confirm_application_accept(user, applier)
     @applier = applier
     mail(:to      => user.email,
+         :bcc => User.with_role(:contributor).pluck(:email),
          :subject => "[RORLAB] #{@applier.schedule.name} 참가 최종 승인")
   end
 
