095996c43056298be09989598170ebca9643c69a
Lucius Choi <lucius.choi@gmail.com>
2016-06-14 16:52:28 +0900

    용어집 일차완성

--- app/models/glossary.rb
+++ app/models/glossary.rb
@@ -2,11 +2,10 @@ class Glossary < ActiveRecord::Base
 
   resourcify
   include Authority::Abilities
-    
+
   belongs_to :user
   has_many :glossary_definitions, dependent: :destroy
 
-  accepts_nested_attributes_for :glossary_definitions, allow_destroy: true
-
+  accepts_nested_attributes_for :glossary_definitions, allow_destroy: true, :reject_if => :all_blank
   validates :term, presence: true
 end
