27fab7526e8a23c9b15e2ec889bf5a058089f400
Lucius Choi <lucius.choi@gmail.com>
2016-06-15 16:15:35 +0900

    용어집 show 액션 뷰에서 정의 추가하기

--- app/controllers/glossary_definitions_controller.rb
+++ app/controllers/glossary_definitions_controller.rb
@@ -1,9 +1,38 @@
 class GlossaryDefinitionsController < ApplicationController
+  before_action :set_glossary
+  before_action :set_glossary_definition, only: [:update, :destroy, :like_toggle]
+
+  def create
+    @definition = @glossary.glossary_definitions.build(glossary_definition_params)
+    @definition.user = current_user
+    @definition.save
+  end
+
+  def update
+    @definition.update(glossary_definition_params)
+  end
+
+  def destroy
+    @definition.destroy
+  end
 
   def like_toggle
-    @glossary = Glossary.find(params[:glossary_id])
-    @definition = @glossary.glossary_definitions.find(params[:id])
     current_user.voted_up_on?(@definition) ?  @definition.unliked_by(current_user) : @definition.liked_by(current_user)
+  end
+
+  private
+
+  def set_glossary
+    @glossary = Glossary.find(params[:glossary_id])
+  end
+
+  def set_glossary_definition
+    set_glossary
+    @definition = @glossary.glossary_definitions.find(params[:id])
+  end
+
+  def glossary_definition_params
+    params.require(:glossary_definition).permit(:id, :definition, :sentence)
   end
 
 end
