1d153fc3518cfa1283e366aa259e742942e4cfe5
Lucius Choi <lucius.choi@gmail.com>
2016-06-17 08:09:55 +0900

    정의 좋아요 클릭한 사용자 목록 툴팁 추가

--- app/helpers/glossaries_helper.rb
+++ app/helpers/glossaries_helper.rb
@@ -1,2 +1,10 @@
 module GlossariesHelper
+  def likers(definition)
+    likers = definition.votes_for.up
+    if likers.size < 11
+      likers.voters.map(&:email).join(', ')
+    else
+      likers.voters[0,10].map(&:email).join(', ') + " 외 #{likers.size - 10 }"
+    end
+  end
 end
