9f4626b273ca134c57afb2f23c450fd87a9965b2
Lucius Choi <lucius.choi@gmail.com>
2016-06-30 21:52:24 +0900

    add_foreign_key 추가

--- db/migrate/20160630124622_create_newsletters.rb
+++ db/migrate/20160630124622_create_newsletters.rb
@@ -1,13 +1,14 @@
 class CreateNewsletters < ActiveRecord::Migration
   def change
     create_table :newsletters do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :greeting
       t.datetime :published_at
-      t.references :editor, index: true, foreign_key: true
+      t.references :editor, index: true
       t.string :poster
 
       t.timestamps null: false
     end
+    add_foreign_key :newsletters, :users, column: :editor_id
   end
 end
