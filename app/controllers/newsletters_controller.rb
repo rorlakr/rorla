fbe2f100e1969d847367b083ae4b197142578f1f
Lucius Choi <lucius.choi@gmail.com>
2016-07-02 14:33:35 +0900

    뉴스레터 인증 추가

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -1,4 +1,5 @@
 class NewslettersController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_newsletter, only: [:show, :edit, :update, :destroy]
 
   # GET /newsletters
