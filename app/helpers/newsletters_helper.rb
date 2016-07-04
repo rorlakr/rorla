be23ff8ce4d7a6842fdbe5a70d3236188d81d56d
Lucius Choi <lucius.choi@gmail.com>
2016-07-04 20:55:33 +0900

    뉴스레터 폼뷰 보기 오류 수정

--- app/helpers/newsletters_helper.rb
+++ app/helpers/newsletters_helper.rb
@@ -1,2 +1,21 @@
 module NewslettersHelper
+  def preview_section( name, index )
+    articles = @newsletter.articles.where( news_section_id: index)
+    content_tag(:tr) do
+      concat content_tag(:th, name, class:'col-md-2 text-right')
+      concat(content_tag(:td, class: 'section_articles') do
+        content_tag(:div, id: "section_articles_#{index}") do
+          concat render(articles)
+        end
+      end)
+    end unless articles.size.zero?
+  end
+
+  def preview_sections
+    content_tag(:table, class:'table table-striped') do
+      %w(행사안내 읽을꺼리 볼꺼리 신간소개 채용공고 업체홍보).each_with_index do | section, index |
+        concat preview_section( '행사안내', index + 1  )
+      end
+    end
+  end
 end
