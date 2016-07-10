1f28d677c60c29b02f3e0fab9c47bb3ea155e5eb
Lucius Choi <lucius.choi@gmail.com>
2016-07-11 00:50:56 +0900

    recaptcha 추가함.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,18 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+
   def create
-    @user = build_resource # Needed for Merit
-    super
+   if !verify_recaptcha
+      flash.delete :recaptcha_error
+      build_resource(sign_up_params)
+      resource.valid?
+      resource.errors.add(:base, "There was an error with the recaptcha code below. Please re-enter the code.")
+      clean_up_passwords(resource)
+      respond_with_navigational(resource) { render_with_scope :new }
+    else
+      flash.delete :recaptcha_error
+      @user = build_resource # Needed for Merit
+      super
+    end
   end
 
   def update
