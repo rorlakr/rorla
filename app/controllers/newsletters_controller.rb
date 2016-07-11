bff0085e0045be250ecdccd9b1d0690e3c4eecae
Shia <rise.shia@gmail.com>
2016-07-11 20:20:22 +0900

    Refactoring NewsletterController

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -1,6 +1,6 @@
 class NewslettersController < ApplicationController
   include ActionView::Helpers::TextHelper
-  before_action :authenticate_user!, except: [:index, :show, :preview, :new_subscribe, :subscribe, :unsubscribe ]
+  before_action :authenticate_user!, except: [:index, :show, :preview, :new_subscribe, :subscribe, :unsubscribe]
   before_action :set_newsletter, only: [:show, :preview, :sendmail, :edit, :update, :destroy]
 
   # GET /newsletters
@@ -8,35 +8,17 @@ class NewslettersController < ApplicationController
   def index
     @newsletters = Newsletter.order( created_at: :desc )
 
-    set_meta_tags og: {
-      title: '뉴스레터',
-      description: "1주간의 루비와 레일스 관련기사를 공개 모집하여 발송하는 신개념의 뉴스레터",
-      image: root_url[0..-2] + ActionController::Base.helpers.asset_url('newsletter.png'),
-      url: newsletters_url
-    }, title: '뉴스레터'
-
+    set_meta_tags default_meta_tags
   end
 
   # GET /newsletters/1
   # GET /newsletters/1.json
   def show
-
-    set_meta_tags og: {
-      title: @newsletter.title,
-      description: truncate(@newsletter.greeting, :length   => 300, :separator => /\w/, :omission => "&hellip;"),
-      image: root_url[0..-2] + ActionController::Base.helpers.asset_url('newsletter.png'),
-      url: preview_newsletter_url(@newsletter)
-    }, title: @newsletter.title
-
+    set_meta_tags build_meta_tags(@newsletter)
   end
 
   def preview
-    set_meta_tags og: {
-      title: @newsletter.title,
-      description: truncate(@newsletter.greeting, :length   => 300, :separator => /\w/, :omission => "&hellip;"),
-      image: root_url[0..-2] + ActionController::Base.helpers.asset_url('newsletter.png'),
-      url: preview_newsletter_url(@newsletter)
-    }, title: @newsletter.title
+    set_meta_tags build_meta_tags(@newsletter)
   end
 
   def sendmail
@@ -100,46 +82,14 @@ class NewslettersController < ApplicationController
 
   def new_subscribe
     if params[:email]
-      begin
-        @user_email = Base64.urlsafe_decode64(params[:email])
-      rescue => e
-        @user_email = params[:email]
-      end
+      @user_email = email_param
     end
   end
 
   def subscribe
-    if params[:user_id]
-      user_email = User.find(params[:user_id]).email
-      if Newsletter.subscribe(user_email)
-        UserMailer.confirm_subscribe_newsletter(user_email).deliver_later
-        flash[:notice] = '구독 신청이 완료되었습니다. 잠시 후 이메일을 확인하시기 바랍니다. '
-      else
-        flash[:notice] = "이미 구독 중입니다."
-      end
-    else
-      if verify_recaptcha
-        flash.delete :recaptcha_error
-        begin
-          user_email = Base64.urlsafe_decode64(params[:email])
-        rescue => e
-          user_email = params[:email]
-        end
-        if result = Newsletter.subscribe(user_email)
-          if result == 'added'
-            UserMailer.confirm_subscribe_newsletter(user_email).deliver_later
-            flash[:notice] = '구독 신청이 완료되었습니다. 잠시 후 이메일을 확인하시기 바랍니다. '
-          else
-            flash[:error] = result
-          end
-        else
-          flash[:notice] = "이미 구독 중입니다."
-        end
-      else
-        flash.delete :recaptcha_error
-        flash[:error] = "로봇이 아니면 로봇여부를 체크해 주세요. "
-      end
-    end
+    flash.delete :recaptcha_error
+    user_email = User.find_by(id: params[:user_id]).email || email_param
+    flash[:notice] = subscribe_user(user_email)
 
     respond_to do |format|
       format.html { redirect_to :back }
@@ -148,17 +98,16 @@ class NewslettersController < ApplicationController
   end
 
   def unsubscribe
-    begin
-      user_email = params[:user_id] ? User.find(params[:user_id]).email : Base64.urlsafe_decode64(params[:email])
-    rescue => e
-      user_email = params[:email]
-    end
-    if Newsletter.unsubscribe(user_email)
-      UserMailer.confirm_unsubscribe_newsletter(user_email).deliver_later
-      flash[:notice] = '구독 해제되었습니다. 잠시 후 이메일을 확인하시기 바랍니다.'
-    else
-      flash[:notice] = "이미 구독해제 상태입니다."
-    end
+    user_email = User.find_by(id: params[:user_id]).email || email_param
+
+    flash[:notice] = \
+      if Newsletter.unsubscribe(user_email)
+        UserMailer.confirm_unsubscribe_newsletter(user_email).deliver_later
+        '구독 해제되었습니다. 잠시 후 이메일을 확인하시기 바랍니다.'
+      else
+        '이미 구독해제 상태입니다.'
+      end
+
     respond_to do |format|
       format.html { redirect_to newsletters_url}
       format.js
@@ -173,6 +122,47 @@ class NewslettersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def newsletter_params
-      params.require(:newsletter).permit(:title, :title_color, :greeting, :published_at, :editor_id, :poster, :remove_poster )
+      params.require(:newsletter)
+        .permit(:title, :title_color, :greeting, :published_at,
+                :editor_id, :poster, :remove_poster)
+    end
+
+    def subscribe_user(user_email)
+      return '로봇이 아니면 로봇여부를 체크해 주세요.' unless verify_recaptcha
+      result = Newsletter.subscribe(user_email) || '이미 구독 중입니다.'
+      if result == 'added'
+        UserMailer.confirm_subscribe_newsletter(user_email).deliver_later
+        '구독 신청이 완료되었습니다. 잠시 후 이메일을 확인하시기 바랍니다.'
+      else
+        result
+      end
+    end
+
+    def email_param
+      Base64.urlsafe_decode64(params[:email])
+    rescue => e
+      params[:email]
+    end
+
+    def default_meta_tags
+      {
+        og: {
+          title: '뉴스레터',
+          description: '1주간의 루비와 레일스 관련기사를 공개 모집하여 발송하는 신개념의 뉴스레터',
+          image: root_url[0..-2] + ActionController::Base.helpers.asset_url('newsletter.png'),
+          url: newsletters_url
+        },
+        title: '뉴스레터'
+      }
+    end
+
+    def build_meta_tags(newsletter)
+      default_meta_tags.merge(
+        og: {
+          title: newsletter.title.presence,
+          description: truncate(newsletter.greeting, length: 300, separator: /\w/, omission: '&hellip;'),
+          image: root_url[0..-2] + ActionController::Base.helpers.asset_url('newsletter.png'),
+          url: preview_newsletter_url(newsletter)
+        }, title: newsletter.title)
     end
 end
