3fc13c0ddaa11cdfac6bd9c63c6920f8ad455b9e
Lucius Choi <lucius.choi@gmail.com>
2018-01-02 15:32:30 +0900

    change location of config.storage in carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,6 @@ end
 # Use AWS storage if in production
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
@@ -17,6 +16,7 @@ if Rails.env.production?
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region:                ENV['AWS_S3_REGION']
     }
+    config.storage = :fog
     config.fog_directory  = ENV['AWS_BUCKET_NAME']
     config.fog_public     = true
     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }      
