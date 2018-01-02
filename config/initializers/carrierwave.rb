b94276f95dd5d3cfcb724a4ff91d370b04fa672a
Lucius Choi <lucius.choi@gmail.com>
2018-01-02 14:32:46 +0900

    fix typos

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -20,7 +20,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],     # required
     :region                 => ENV['AWS_S3_REGION']                        # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = ENV['AWS_BUCKET_NAEM']               # required
+  config.fog_directory  = ENV['AWS_BUCKET_NAME']               # required
   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
   #config.fog_public     = false                                  # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
