4a149642be105e47622de4a6c326e590e0af7f8b
Lucius Choi <lucius.choi@gmail.com>
2018-01-02 15:28:50 +0900

    fixed carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,31 +1,26 @@
-CarrierWave.configure do |config|
 
-  # Use local storage if in development or test
-  if Rails.env.development? || Rails.env.test?
-    CarrierWave.configure do |config|
-      config.storage = :file
-    end
+# Use local storage if in development or test
+if Rails.env.development? || Rails.env.test?
+  CarrierWave.configure do |config|
+    config.storage = :file
   end
+end
 
-  # Use AWS storage if in production
-  if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.storage = :fog
-    end
+# Use AWS storage if in production
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region:                ENV['AWS_S3_REGION']
+    }
+    config.fog_directory  = ENV['AWS_BUCKET_NAME']
+    config.fog_public     = true
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }      
   end
-
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        # required
-    region:                ENV['AWS_S3_REGION'],                  # optional, defaults to 'us-east-1'
-    # host:                  's3.example.com',             # optional, defaults to nil
-    # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
-  }
-  config.fog_directory  = ENV['AWS_BUCKET_NAME']                                   # required
-  config.fog_public     = false                                                 # optional, defaults to true
-  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
 end
 
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\s\.\-\+]/
