b50ab0a4087445eccd1ec71a5ae51e2e96197a58
Lucius Choi <lucius.choi@gmail.com>
2018-01-02 15:55:48 +0900

    so difficult to setup S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,25 +1,18 @@
-# Use local storage if in development or test
-if Rails.env.development? || Rails.env.test?
-  CarrierWave.configure do |config|
-    config.storage = :file
-  end
-end
+CarrierWave.configure do |config|
 
-# Use AWS storage if in production
-if Rails.env.production?
-  CarrierWave.configure do |config|
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      # Configuration for Amazon S3
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region                => ENV['AWS_S3_REGION']
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_S3_REGION'],
+      endpoint: 'https://s3.amazonaws.com'
     }
-  
-    config.cache_dir = "#{Rails.root}/tmp/uploads"
-    config.fog_directory  = ENV['AWS_BUCKET_NAME']
-    config.fog_public     = false
-    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
+    config.fog_directory = ENV['AWS_BUCKET_NAME']
+    config.fog_public = false
     config.storage = :fog
   end
 end
