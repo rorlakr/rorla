66eeb55763d0cd1b966393a3e38b13c90a4463e6
Lucius Choi <lucius.choi@gmail.com>
2018-01-02 14:25:43 +0900

    update carriwerwave uploader class to fog

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,7 +7,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
