310f3e397a9b1bbbdcabb6caf771399712a91e5b
Lucius Choi <lucius.choi@gmail.com>
2018-01-02 14:28:54 +0900

    fixed carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,11 +16,11 @@ CarrierWave.configure do |config|
   
   config.fog_credentials = {
     :provider               => 'AWS',                             # required
-    :aws_access_key_id      => "<%= ENV['AWS_ACCESS_KEY_ID'] %>",            # required
-    :aws_secret_access_key  => "<%= ENV['AWS_SECRET_ACCESS_KEY'] %>",     # required
-    :region                 => "<%= ENV['AWS_S3_REGION'] %>"                        # optional, defaults to 'us-east-1'
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],     # required
+    :region                 => ENV['AWS_S3_REGION']                        # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = "<%= ENV['AWS_BUCKET_NAEM'] %>"               # required
+  config.fog_directory  = ENV['AWS_BUCKET_NAEM']               # required
   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
   #config.fog_public     = false                                  # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
