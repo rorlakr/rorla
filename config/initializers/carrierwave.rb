81d5172b15c43153a0699e7ca6a25b85289f0ee0
Lucius Choi <lucius.choi@gmail.com>
2018-01-02 15:41:33 +0900

    zzz

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,7 +10,7 @@ end
 # Use AWS storage if in production
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
+    # config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
