f07d2e4c04699c9d68c15eca275157429389e2ba
Lucius Choi <lucius.choi@gmail.com>
2018-01-31 23:06:33 +0900

    revert

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.default_url_options[:host] = 'rorlab.org'
 
 Rails.application.routes.draw do
-  match "*path" => redirect("https://www.rorlab.org/%{path}"), :constraints => { :protocol => "http://" }, via: [ :get, :post ]
-  match "*path" => redirect("https://www.rorlab.org/%{path}"), :constraints => { :subdomain => "" }, via: [ :get, :post ]
 
   root "welcome#index"
 
