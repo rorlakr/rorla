1ed09f8e8f21ede5ecd9b98ee01890b832fa4679
Lucius Choi <lucius.choi@gmail.com>
2018-02-04 12:26:26 +0900

    added migration version

--- db/migrate/20180202113355_add_params_to_impressions.rb
+++ db/migrate/20180202113355_add_params_to_impressions.rb
@@ -1,4 +1,4 @@
-class AddParamsToImpressions < ActiveRecord::Migration
+class AddParamsToImpressions < ActiveRecord::Migration[5.2]
   def change
     add_column :impressions, :params, :text
     add_index :impressions, [:impressionable_type, :impressionable_id, :params], name: 'poly_params_request_index', unique: false, length: { params: 100 }
