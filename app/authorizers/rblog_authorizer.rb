7f43c08dacffa48064a205337d05143f0cc749f9
Lucius Choi <lucius.choi@gmail.com>
2018-02-05 05:10:39 +0900

    Contributor role only can create a rBlog from now

--- app/authorizers/rblog_authorizer.rb
+++ app/authorizers/rblog_authorizer.rb
@@ -1,7 +1,7 @@
 class RblogAuthorizer < ApplicationAuthorizer
 
   def self.creatable_by?(user, options={})
-    user.has_role?(:user) || user.has_role?(:admin)
+    user.has_role?(:contributor) || user.has_role?(:admin)
   end
 
   def updatable_by?(user)
