49c2ec287cdb7df6511478447de0a346a8ebe2bd
Lucius Choi <lucius.choi@gmail.com>
2018-02-04 22:14:00 +0900

    revert

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,8 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   config.require_master_key = true
 
+  config.secret_key_base = Rails.application.credentials.dig(:production, :secret_key_base)
+  
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
@@ -113,6 +115,5 @@ Rails.application.configure do
 
   config.action_mailer.asset_host = "https://www.rorlab.org"
 
-  # config.secret_key_base = Rails.application.credentials.dig(:production, :secret_key_base)
 
 end
