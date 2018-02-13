3f08da0882a58fffa62eac204fbde0b02da7d6ad
Lucius Choi <lucius.choi@gmail.com>
2018-02-14 06:33:59 +0900

    rails-env-credentials 세팅완료

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 Recaptcha.configure do |config|
-  config.site_key  = Rails.application.credentials.dig(:recaptcha, :public_key)
+  config.site_key = Rails.application.credentials.dig(:recaptcha, :public_key)
   config.secret_key = Rails.application.credentials.dig(:recaptcha, :private_key)
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
