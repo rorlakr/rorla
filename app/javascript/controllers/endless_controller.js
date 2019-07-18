3141f8167cdbbb59b17df4c38e3ec574a9a77e10
Lucius Choi <lucius.choi@gmail.com>
2019-07-18 22:06:40 +0900

    refactored endless_controller.js

--- app/javascript/controllers/endless_controller.js
+++ app/javascript/controllers/endless_controller.js
@@ -7,14 +7,12 @@ export default class extends Controller {
     $('.pagination').hide();
     if ($(".pagination").length && $(this).length) {
       $(window).scroll(function() {
-        var url;
-        url = $(".pagination a[rel='next']").attr("href");
+        let url = $(".pagination a[rel='next']").attr("href");
         if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-          $(".pagination").text("Loading more...");
+          // $(".pagination").text("Loading more...");
           return $.getScript(url);
         }
       });
-      return $(window).scroll();
     }
   }
 }
