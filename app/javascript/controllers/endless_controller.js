5dc1d1a7a98bb97df476b62f8576b394f004803d
Lucius Choi <lucius.choi@gmail.com>
2019-07-19 00:39:13 +0900

    added loading messages

--- app/javascript/controllers/endless_controller.js
+++ app/javascript/controllers/endless_controller.js
@@ -9,7 +9,15 @@ export default class extends Controller {
       $(window).scroll(function() {
         let url = $(".pagination a[rel='next']").attr("href");
         if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-          // $(".pagination").text("Loading more...");
+          $('.loading_message').html(`
+          <div class='h3 text-center text-primary'>Loading more....</div>
+          <div class="text-center my-3">
+            <div class="spinner-border text-primary" role="status">
+              <span class="sr-only">Loading...</span>
+            </div>
+          </div>
+          `)
+          sleep(5);
           return $.getScript(url);
         }
       });
